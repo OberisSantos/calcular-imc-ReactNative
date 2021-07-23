b24b46d52de59fb13a68107f728d178501980b4b
unknown <oberis2010@gmail.com>
2021-07-22 23:44:44 -0300

    Calculo automático e correção de bugs

--- App.js
+++ App.js
@@ -1,79 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import styled from 'styled-components/native';
 
 export default function App() {
     const [peso, alteraPeso] = useState();
     const [altura, alteraAltura] = useState();
-    const [imc, alteraImc] = useState("0");
+    const [imc, alteraImc] = useState(0);
     const [grau, alterarGrau] = useState("");
     const [classificacao, alteraClassificacao] = useState("");
-    const [cor, alteraCor] = useState('#c3c3c3');
+    const [cor, alteraCor] = useState('#D1B514');
 
-    function calcular(){
-      const imc = parseFloat(peso / (altura * altura));
-      if (imc <= 18.5) {
-        return(          
-          alteraImc(imc),
-          alterarGrau("Grau 0"),
-          alteraClassificacao("Magreza"),
-          alteraCor('#D1B514')
-        )        
-        
-      } if (imc > 18.5 && imc <= 24.9) {
-          return(
-            alteraImc(imc),
-            alterarGrau("Grau 0"),
-            alteraClassificacao("Normal"),
-            alteraCor('#599992')
-          )
-      } if (imc > 24.9 && imc <= 29.9) {
-          return(
-            alteraImc(imc),
-            alterarGrau("Grau I"),
-            alteraClassificacao("Sobrepeso"),
-            alteraCor('#BB9599')
-          )
-      } if (imc > 29.9 && imc <= 39.9) {
-          return(
-            alteraImc(imc),
-            alterarGrau("Grau II"),
-            alteraClassificacao("Obesidade"),
-            alteraCor('#B45714')
-          )
-      } if (imc > 39.9) {
-          return(
-            alteraImc(imc),
-            alterarGrau("Grau III"),
-            alteraClassificacao("Obesidade Grave"),
-            alteraCor('#8C1A24')
-          )      
+    const calcular = () => {
+      
+      if (altura > 0) {
+        const cal = peso / (altura * altura);
+        alteraImc((cal).toFixed(1));
+      
+
+        if (cal <= 18.5) {    
+            alterarGrau("0");
+            alteraClassificacao("Magreza");
+            alteraCor('#D1B514');
+                
+          
+        } else if (cal > 18.5 && cal <= 24.9) {
+              alterarGrau("0");
+              alteraClassificacao("Normal");
+              alteraCor('#599992');
+            
+        } else if (cal > 24.9 && cal <= 29.9) {
+              alterarGrau("I");
+              alteraClassificacao("Sobrepeso");
+              alteraCor('#BB9599');
+            
+        } else if (cal > 29.9 && cal <= 39.9) {
+              alterarGrau("II");
+              alteraClassificacao("Obesidade");
+              alteraCor('#B45714');
+            
+        } else if (cal > 39.9) {
+              alterarGrau("III");
+              alteraClassificacao("Obesidade Grave");
+              alteraCor('#8C1A24');
+            
+        }
       }else{
-        return(
-          alteraImc(imc),
-          alterarGrau(""),
-          alteraClassificacao(""),
-          alteraCor('#c3c3c3')
-        )
+        alteraImc(0);
       }
-
     }
-
+    {/*Chama a função automaticamente */}
+    useEffect (() => { calcular() }, [altura, peso]);
     
   return (
     <Tela>
       <Titulo>Calcular IMC</Titulo>
       <Input keyboardType="numeric" value= {peso} onChangeText={(p) => {alteraPeso(p)}} placeholder="Infomar o peso em kg"/>
       <Input keyboardType="numeric" value={altura}  onChangeText={(a) => {alteraAltura(a)}} placeholder="Infomar a altura em m"/>
-      <BtnView>
-        <BtnCalcular title="Calcular" onPress={() => {calcular()}} color="#54A0A5"/>
-      </BtnView>
       
-      <Resultado style={{backgroundColor:cor}}>
-        <TituloResultado>IMC: {parseFloat(imc).toFixed(2)}  {grau}</TituloResultado>
-        <SubResultado>{classificacao}</SubResultado>
-      </Resultado>
+      {/* Essa parte foi removida para ficar automática o calculo
+      <BtnView>
+        <BtnCalcular title="Calcular" onPress={calcular} color="#54A0A5"/>
+      </BtnView>
+      */}
+
+      { imc > 0 && (
+        <Resultado cor={cor}>
+          <TituloResultado>IMC: {imc} Grau: {grau}</TituloResultado>
+          <SubResultado>{classificacao}</SubResultado>
+        </Resultado>)
+      }
 
     </Tela>
   );
@@ -83,7 +79,7 @@ const Resultado = styled.View`
   width: 70%;
   margin-top: 35px;
   height: 89px;
-  backgroundColor:orange;
+  background-color:${props => props.cor};
   align-items: center;
   padding-top: 5px;
 
@@ -127,7 +123,7 @@ const Titulo = styled.Text`
 const Input = styled.TextInput`
   width: 90%;
   height: 40px;
-  backgroundColor: #E3E2E2;
+  background-color: #E3E2E2;
   border-radius: 5px;
   margin-top: 10px;
   padding-left: 10px;
